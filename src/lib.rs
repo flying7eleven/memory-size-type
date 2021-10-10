251e56e8f7e399fa655789f2775cdb0d4a2d733b
Tim Janke <tim+github@janke.biz>
2021-10-10 17:26:54 +0200

    :sparkles: Implement std::fmt::Debug for byte

--- src/lib.rs
+++ src/lib.rs
@@ -70,6 +70,29 @@ impl std::fmt::Display for Byte {
     }
 }
 
+#[cfg(feature = "std")]
+impl std::fmt::Debug for Byte {
+    /// Formats the represented byte value using the given formatter in a programmer-facing,
+    /// debugging context.
+    ///
+    /// # Example
+    /// ```
+    /// use memory_size_type::Byte;
+    ///
+    /// let one_byte = Byte::from(1);
+    /// let several_bytes = Byte::from(256);
+    ///
+    /// assert_eq!("1 byte", format!("{}", one_byte));
+    /// assert_eq!("256 bytes", format!("{}", several_bytes));
+    /// ```
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.0 == 1 {
+            return write!(f, "{} byte", self.0);
+        }
+        write!(f, "{} bytes", self.0)
+    }
+}
+
 #[cfg(feature = "deprecated")]
 pub struct MemorySize {
     size: u64,
